a4628bbd9f1225bbd102767ef64c730f1206fff1
cnb.monster <cnb.monster@gmail.com>
2023-01-21 22:11:30 +0900

    submenu error solved by element click

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -1,7 +1,16 @@
 function spread(count){
-    document.getElementById('folder-checkbox-' + count).checked =
-    !document.getElementById('folder-checkbox-' + count).checked
-    document.getElementById('spread-icon-' + count).innerHTML =
-    document.getElementById('spread-icon-' + count).innerHTML == 'arrow_right' ?
-    'arrow_drop_down' : 'arrow_right'
+    document.getElementById('folder-checkbox-' + count).checked = !document.getElementById('folder-checkbox-' + count).checked
+    document.getElementById('spread-icon-' + count).innerHTML = document.getElementById('spread-icon-' + count).innerHTML == 'arrow_right' ? 'arrow_drop_down' : 'arrow_right'
+}
+
+window.onload = function(ev){
+    var checkboxes = document.querySelectorAll('input[id^="folder-checkbox"]')
+    checkboxes.forEach(function(elem){
+        elem.checked = true
+
+        var count = elem.id.split('-').at(-1)
+        var icon = document.getElementById('spread-icon-' + count)
+        icon.click()
+    })
+
 }
